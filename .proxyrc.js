4211491dfc6dd5c9ae24e7388f2245e7839fbe11
Vladimir Grichina <vgrichina@gmail.com>
2022-02-17 02:31:21 -0700

    Use web4-near directly

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,14 +1,18 @@
 const serveStatic = require('serve-static')
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const web4 = require('web4-near')
 
 module.exports = function (app) {
     // Use static middleware
     app.use(serveStatic('static'))
 
     // Proxy to web4
-    app.use(
-        createProxyMiddleware("/web4", {
-            target: "http://localhost:3000/"
-        })
-    );
+    const web4Callback = web4.callback();
+    app.use((req, res, next) => {
+        if (req.url.startsWith('/web4')) {
+            web4Callback(req, res, next);
+            return;
+        }
+
+        next();
+    })
 }
